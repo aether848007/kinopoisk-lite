71589bdaa0df5fe10dccd1ffafef9cc2e29e8908
aether848007 <bekaaliyev848007@gmail.com>
2025-06-29 20:56:31 +0500

    Применение класса Route

---
+++ src/Router/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Router;
+
+class Router
+{
+    private array $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
+
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
+
+    public function dispatch(string $uri): void
+    {
+        $routes = $this->getRoutes();
+
+        $routes[$uri]();
+    }
+
+    private function initRoutes()
+    {
+        $routes = $this->getRoutes();
+
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUri()] = $route;
+        }
+
+        dd($this->routes);
+    }
+
+
+    /**
+     * @return Route[]
+     */
+    private function getRoutes(): array
+    {
+        return require_once APP_PATH . '/config/routes.php';
+    }
+}
